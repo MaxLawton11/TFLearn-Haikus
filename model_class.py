9fd72d14b849a3312140915a0a27e5ae46434ba4
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-24 19:50:07 -0400

    Update model_class.py

--- model_class.py
+++ model_class.py
@@ -1,1 +1,35 @@
+import tflearn
+from tflearn.data_utils import *
 
+class Model:
+    def __init__(self, path: str):        
+        # load and preprocess the text data
+        self.path = path
+        self.maxlen = 25
+        self.char_idx = None
+        self.X, self.Y, self.char_idx = textfile_to_semi_redundant_sequences(self.path, seq_maxlen=self.maxlen, redun_step=1)
+
+        # define the network architecture
+        input_layer = tflearn.input_data(shape=[None, self.maxlen, len(self.char_idx)])
+        lstm_layer = tflearn.lstm(input_layer, 256)
+        output_layer = tflearn.fully_connected(lstm_layer, len(self.char_idx), activation='softmax')
+        self.net = tflearn.regression(output_layer, optimizer='adam',
+                                loss='categorical_crossentropy')
+        # create the model
+        self.model = tflearn.SequenceGenerator(self.net, dictionary=self.char_idx,
+                                        seq_maxlen=self.maxlen,
+                                        clip_gradients=5.0,
+                                        checkpoint_path='model.ckpt')
+        
+    def train(self, n_epoch: int) :
+        # train the model
+        self.model.fit(self.X, self.Y, validation_set=0.1, batch_size=128,
+                n_epoch=n_epoch, run_id='text_generation')
+        
+    def generate(self, length: int, seed: str, temperature=0.5) :
+        # generate text
+        return self.model.generate(length, temperature=temperature, seq_seed=seed)
+    
+    def save(self, filename: str):
+        # save the trained model to a file
+        self.model.save(filename)
