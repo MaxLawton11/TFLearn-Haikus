41c353f5e4ac07c1f481645651eb0cc8d1133afe
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-24 20:00:20 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,1 +1,29 @@
+from Model import *
+import os
+import os.path
 
+path = 'haiku_dataset.text'
+seed = "our destination"
+temp = .5
+n_chars = 100
+
+print('--------- Generating ---------')
+# test for vaild model
+if os.path.isfile('model_instance.tflearn.index') and os.path.isfile('model_instance.tflearn.meta') :
+    m.model.load('model_instance.tflearn')
+    print('# Loaded  model')
+else :
+    print('# No model found. Please train before generating.')
+
+# load model
+m = Model(path)
+m.model.load('model_instance.tflearn')
+
+# make text from seed
+text = m.generate(n_chars, seed, temp)
+print(f'Seed: "{seed}"')
+print(f'n_chars: "{n_chars}"')
+print('Text: ', text)
+del m
+
+print('--------- Success ---------')
