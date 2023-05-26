e11ef9931dfcccdcb6515735d8fc57f4200d121b
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-26 11:38:24 -0400

    Update model_class.py

--- model_class.py
+++ model_class.py
@@ -26,7 +26,7 @@ class Model:
         self.model.fit(self.X, self.Y, validation_set=0.1, batch_size=128,
                 n_epoch=n_epoch, run_id='haiku')
         
-    def generate(self, seed: str, length: int = 25, temperature=0.5) :
+    def generate(self, seed: str, length: int, temperature: float) :
         # generate text
         return self.model.generate(length, temperature=temperature, seq_seed=seed)
     
