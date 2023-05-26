fb69c3b59b9e43363a5e165b4a570c03e8f00cfe
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-26 11:43:03 -0400

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import os.path
 
 ds_path = 'haiku_dataset.text'
+model_path = 'model_instance.tflearn'
 n_epoch = 10
 
 print('--------- Training ---------')
@@ -12,15 +13,15 @@ print(f'# Running {n_epoch} epoch(s)')
 m = Model(ds_path)
 
 # load model if there is one
-if os.path.isfile('model_instance.tflearn.index') and os.path.isfile('model_instance.tflearn.meta') :
-    m.model.load('model_instance.tflearn')
-    print('# Loaded pre-existing model')
+if os.path.isfile(f'{model_path}.index') and os.path.isfile(f'{model_path}.meta') :
+    m.model.load(model_path)
+    print(f'# Loaded pre-existing model ({model_path})')
 else :
     print('# No model found - Creating new model')
    
 # train and save
 m.train(n_epoch)
-m.save('model_instance.tflearn')
+m.save(model_path)
 del m
 
 print('--- Model Traind & Saved ---')
