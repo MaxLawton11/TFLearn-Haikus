f525210dd985e0a321d6d02c926d1178a949d5c7
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-30 14:46:14 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ import os.path
 
 ds_path = 'haiku_dataset.text'
 model_path = 'model_instance.tflearn'
-seed = "a sultry wine night /  spent with a beautiful girl /  brash kiss ends smooth jazz $"
+seed = "test of faith / today i"
 temp = 0.01
 n_chars = 100
 
