ecd2e9145bf7302e34fcfb6475aa9f9212a31645
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-31 09:05:31 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,7 +11,7 @@ n_chars = 100
 
 # get random line from file
 file_lines = open(ds_path).read().splitlines()
-random_line = random.choice(lines)
+random_line = random.choice(file_lines)
 seed = random_line[0:26] # only get first 25chars and set to seed
 del file_lines; del random_line;
 
