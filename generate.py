294cfd6e4d33a2ed710d9ba10561dd177134df45
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-30 22:08:33 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,7 +5,7 @@ import os.path
 ds_path = 'haiku_dataset.text'
 model_path = 'model_instance.tflearn'
 seed = "test of faith / today i "
-temp = .99
+temp = .01
 n_chars = 100
 
 print('--------- Generating ---------')
