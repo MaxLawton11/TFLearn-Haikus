ecda49f211c93b81506ad316785fa4043432b363
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-06-08 11:48:10 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ import random
 ds_path = 'haiku_dataset.text'
 model_path = 'model_instance.tflearn'
 live_display = False
-temp = .01
+temp = .1
 n_chars = 300
 
 # enter any seed under 25 characters long, any more will be cut off!
